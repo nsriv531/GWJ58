ce39fd3d2dd3bf8b427499829f9ced90beb30d75
nashdashin <srivastavanikhil226@yahoo.ca>
2023-06-17 21:38:25 -0600

    powerup noise

--- Objects/Elixer.gd
+++ Objects/Elixer.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 @export var health_amount = 0.5
+@onready var powerup_noise = $PowerUp 
 var can_heal_player:bool = true
-
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -10,12 +10,12 @@ func _on_body_entered(body: Node2D) -> void:
 		if(body.hearts < 3) and can_heal_player:
 			can_heal_player = false
 			body.restore_hearts(0.5)
+			if not powerup_noise.playing:
+				powerup_noise.play()
 			$Timer.start()
 			hide()
-
 			
 	pass # Replace with function body.
-
 
 func _on_timer_timeout() -> void:
 	can_heal_player = true
